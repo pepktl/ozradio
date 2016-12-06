96795b1c30b13c345b92e578eb40b49eb3d16bb4
Unknown <eko.prasetya261@gmail.com>
2016-12-06 14:15:40 +0800

    Edit List
    Edit list news & ponggawa, edit navbar

--- ponggawa.php
+++ ponggawa.php
@@ -37,98 +37,66 @@
 		<!-- =================================================== -->
 		<!-- CONTENT -->
 		<div id="content">
-                <div class="container background-white" style="width: 100%">
-                    
-                    <div class="row">
-                        <div class="col-md-12">
-                            
-                        </div>
-                    </div>
-
-                    <div class="row" style="margin: 0px auto;">
-                        <div class="col-md-12">
-                            
-                            <!-- Left Item -->
-                                <div class="col-md-3 col-md-offset-3" style="width: 320px; right: 160px; margin-top: 250px;">
-                                    <h3>WELCOME!</h3><h3> YOU WILL LOVE IT</h3>
-                                    <span>PONGGAWA, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</span>
-                                </div>
-                            <!-- End Left Item -->
-
-                            <!-- Right Item -->
-                            <div class="col-md-6 margin-bottom-40">
-                                <div class="col-md-6">
-					  				<div class="row">
-					  					
-					  					<div class="col-md-4"">
-					  						<a href=""><img src="images/pong/190/an.jpg" style="display: inline-block;"></a>
-					  					</div>
-
-					  					<div class="col-md-4">
-					  						<a href=""><img src="images/pong/190/ap.jpg" style="display: inline-block; padding-left: 85px;"></a>
-					  					</div>
-					  					
-					  					<div class="col-md-4">
-					  						<a href=""><img src="images/pong/190/as.jpg" style="display: inline-block; padding-left: 170px;"></a>
-					  					</div>
-
-									</div>
-
-									<div class="row">
-					  					
-					  					<div class="col-md-4">
-					  						<a href=""><img src="images/pong/190/bm.jpg" style="display: inline-block;"></a>
-					  					</div>
-
-					  					<div class="col-md-4">
-					  						<a href=""><img src="images/pong/190/da.jpg" style="display: inline-block; padding-left: 85px;"></a>
-					  					</div>
-					  					
-					  					<div class="col-md-4">
-					  						<a href=""><img src="images/pong/190/dp.jpg" style="display: inline-block; padding-left: 170px;"></a>
-					  					</div>
-					  					
-									</div>
-
-									<div class="row">
-					  					
-					  					<div class="col-md-4">
-					  						<a href=""><img src="images/pong/190/ds.jpg" style="display: inline-block;"></a>
-					  					</div>
-
-					  					<div class="col-md-4">
-					  						<a href=""><img src="images/pong/190/eb.jpg" style="display: inline-block; padding-left: 85px;"></a>
-					  					</div>
-					  					
-					  					<div class="col-md-4">
-					  						<a href=""><img src="images/pong/190/ep.jpg" style="display: inline-block; padding-left: 170px;"></a>
-					  					</div>
-					  					
-									</div>
-
-									<div class="row">
-					  					
-					  					<div class="col-md-4">
-					  						<a href=""><img src="images/pong/190/kl.jpg" style="display: inline-block;"></a>
-					  					</div>
-
-					  					<div class="col-md-4">
-					  						<a href=""><img src="images/pong/190/nv.jpg" style="display: inline-block; padding-left: 85px;"></a>
-					  					</div>
-					  					
-					  					<div class="col-md-4">
-					  						<a href=""><img src="images/pong/190/pc.jpg" style="display: inline-block; padding-left: 170px;"></a>
-					  					</div>
-					  					
-									</div>
-					  			</div>
+            <div class="container background-white" style="width: 100%">
+                <div class="row">
+                    <div class="col-md-12" style="height: 760px;">
+                        
+                        <!-- Left Item -->
+                            <div class="col-md-3 col-md-offset-3" style="width: 320px; right: 160px; margin-top: 250px;">
+                                <h3>WELCOME!</h3><h3> YOU WILL LOVE IT</h3>
+                                <span>PONGGAWA, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</span>
                             </div>
-                            <!-- End Right Item -->
+                        <!-- End Left Item -->
 
+                        <!-- Right Item -->
+                        <div class="col-md-6">
+                            <div class="col-md-12">
+                            	<ul class="ponggawa-ul list-unstyled">
+						    		<li class="ponggawa-list">
+						    			<a href=""><img src="images/pong/190/an.jpg"></a>
+						    		</li>
+						    		<li class="ponggawa-list">
+						    			<a href=""><img src="images/pong/190/ap.jpg"></a>
+						    		</li>
+						    		<li class="ponggawa-list">
+						    			<a href=""><img src="images/pong/190/as.jpg"></a>
+						    		</li>
+						    		<li class="ponggawa-list">
+						    			<a href=""><img src="images/pong/190/bm.jpg"></a>
+						    		</li>
+						    		<li class="ponggawa-list">
+						    			<a href=""><img src="images/pong/190/da.jpg"></a>
+						    		</li>
+						    		<li class="ponggawa-list">
+						    			<a href=""><img src="images/pong/190/dp.jpg"></a>
+						    		</li>
+						    		<li class="ponggawa-list">
+						    			<a href=""><img src="images/pong/190/ds.jpg"></a>
+						    		</li>
+						    		<li class="ponggawa-list">
+						    			<a href=""><img src="images/pong/190/eb.jpg"></a>
+						    		</li>
+						    		<li class="ponggawa-list">
+						    			<a href=""><img src="images/pong/190/ep.jpg"></a>
+						    		</li>
+						    		<li class="ponggawa-list">
+						    			<a href=""><img src="images/pong/190/kl.jpg"></a>
+						    		</li>
+						    		<li class="ponggawa-list">
+						    			<a href=""><img src="images/pong/190/nv.jpg"></a>
+						    		</li>
+						    		<li class="ponggawa-list">
+						    			<a href=""><img src="images/pong/190/pc.jpg"></a>
+						    		</li>
+						    	</ul>
+				  			</div>
                         </div>
+                        <!-- End Right Item -->
+
                     </div>
                 </div>
             </div>
+        </div>
             
 		<div class="divider-page"></div>
 
